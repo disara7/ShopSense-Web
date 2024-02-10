132a148bca7a51f17aa0296955b7cd2afeaddcb4
disara7 <129250994+disara7@users.noreply.github.com>
2024-02-10 14:28:56 +0530

    added nav list

--- shopsense_seller/src/Components/Navbar/Navbar.jsx
+++ shopsense_seller/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,15 @@ import './navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        Nav
+        <ul className='list'>
+          <li>SELLER PORTAL</li>
+          <div className="left">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Join</li>
+          </div>
+        </ul>
     </div>
   )
 }
