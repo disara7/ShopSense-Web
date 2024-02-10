fa0828f811680653f4e0dcc2ca9dac69d32ff912
disara7 <129250994+disara7@users.noreply.github.com>
2024-02-10 14:48:20 +0530

    hero content adjusted

--- shopsense_seller/src/Components/Hero/Hero.jsx
+++ shopsense_seller/src/Components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './hero.css'
 import logo from '../Assets/logo.png'
+import { MdNavigateNext } from "react-icons/md";
+
 
 const Hero = () => {
   return (
     <div className='hero'>
+      <h1>ONLINE SHOPPING FOR THE BLIND</h1>
         <div className="image">
             <img src={logo} alt="" />
         </div>
-        <button>Become a Seller</button>
-        <button>Login</button>
+        <p>ShopSense elevates online shopping experience to blind people through 
+          “taps” on the screen, with artificial intelligence assisting you 
+          selecting your perfect fit with comprehensive narrating.</p>
+        <button>ADD YOUR PRODUCT <MdNavigateNext /></button>
     </div>
   )
 }
