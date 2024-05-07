853d0e5e2015a744ee57a95102bcdface6ad8072
Minuri <92873876+minuri12@users.noreply.github.com>
2024-05-07 18:05:00 +0530

    Cards completed

--- shopsense_seller/src/App.js
+++ shopsense_seller/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Join_section from "./Components/Join_section/join";
+import Card from "./Components/Card/card";
+import Instructions from "./Components/Card/cardData.js";
 
 function App() {
+  const Cards = Instructions.map((card) => {
+    return <Card card={card} />;
+  });
+
   return (
     <div>
       <Navbar />
       <Hero />
       <Join_section />
+
+      <div className="card_holder">{Cards}</div>
     </div>
   );
 }
--- shopsense_seller/src/Components/Join_section/join.jsx
+++ shopsense_seller/src/Components/Join_section/join.jsx
@@ -4,28 +4,34 @@ import Join_img from "../Assets/join_img.jpg";
 
 const Join_section = () => {
   return (
-    <div className="Join_section">
-      <div className="content_section">
-        <div className="Header_text">
-          {" "}
-          GET START WITH <br />
-          SHOPSENSE !
+    <div>
+      <div className="Join_section">
+        <div className="content_section">
+          <div className="Header_text">
+            {" "}
+            GET START WITH <br />
+            SHOPSENSE !
+          </div>
+          <br />
+          <div className="text_box">
+            ShopSense elevates online shopping experience to blind people
+            through “taps” on the screen, with artificial intelligence assisting
+            you selecting your perfect fit with comprehensive narrating.
+          </div>
+          <br />
+          <br />
+          <div className="Button_section">
+            <button className="Join_btn">Join</button>
+            <button className="Signin_btn">Sign In</button>
+          </div>
         </div>
-        <br />
-        <div className="text_box">
-          ShopSense elevates online shopping experience to blind people through
-          “taps” on the screen, with artificial intelligence assisting you
-          selecting your perfect fit with comprehensive narrating.
-        </div>
-        <br />
-        <br />
-        <div className="Button_section">
-          <button className="Join_btn">Join</button>
-          <button className="Signin_btn">Sign In</button>
+        <div className="Image_section">
+          <img src={Join_img} alt="" className="Join_img" />
         </div>
       </div>
-      <div className="Image_section">
-        <img src={Join_img} alt="" className="Join_img" />
+      <div className="Card_header">
+        Set up your e-commerce store in a flash,{" "}
+        <span className="Highlight">it's easy and free!</span>
       </div>
     </div>
   );
