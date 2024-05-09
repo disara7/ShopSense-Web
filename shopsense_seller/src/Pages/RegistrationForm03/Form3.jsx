f4eca7876d7e1d8570979e10abc2ad4f96eaa39b
Minuri <92873876+minuri12@users.noreply.github.com>
2024-05-09 10:11:17 +0530

    Form03 completed

--- shopsense_seller/src/Pages/RegistrationForm03/Form3.jsx
+++ shopsense_seller/src/Pages/RegistrationForm03/Form3.jsx
@@ -3,7 +3,7 @@ import "./Form03.css";
 import CompleteBar from "../../Components/CompleteBar/CompleteBar";
 import FormNavBar from "../../Components/FormNavBar/FormNavBar";
 import { useState } from "react";
-import FileUpload from "../../Components/FileUpload/FileUpload";
+import FileUploadComponent from "../../Components/FileUpload/FileUpload";
 function Form03() {
   return (
     <div>
@@ -11,41 +11,62 @@ function Form03() {
         <div className="formheader">
           Hey Seller, How about we get to know you better?
         </div>
-        <div className="uploadfileholderform3">
-          <FileUpload />
-        </div>
 
         <form>
           <h2>Verify Identification Card</h2>
-          <div className="IdUploadHolder"></div>
+
+          <div className="uploadfileholderform3">
+            <div>
+              <div>ID Card Front Side</div>
+              <FileUploadComponent />
+            </div>
+            <div>
+              <div>ID Card Back Side</div>
+              <FileUploadComponent />
+            </div>
+          </div>
+
           <div className="user__details">
             <div class="input__box">
-              <span class="details">Your Store Name</span>
               <input
                 type="text"
-                placeholder="Enter Store Name"
+                placeholder="Name on the ID card"
                 required
               ></input>
             </div>
             <div class="input__box">
-              <span class="details">Country</span>
+              <input type="text" placeholder="ID Number" required></input>
             </div>
+          </div>
+
+          <h2>Verify Bank Account</h2>
+          <p>
+            Upload the Front Page for Bankbook/Bank Statement/Cheque Copy/Mobile
+            Banking Screenshot
+          </p>
+
+          <FileUploadComponent />
+
+          <div className="user__details">
             <div class="input__box">
-              <span class="details">Your Mobile Number</span>
               <input
                 type="text"
-                placeholder="Enter Mobile Number"
+                placeholder="Account Holder Name"
                 required
               ></input>
+            </div>
+            <div class="input__box">
+              <input type="text" placeholder="Bank Name" required></input>
+            </div>
+            <div class="input__box">
+              <input type="text" placeholder="Branch Name" required></input>
+            </div>
+            <div class="input__box">
+              <input type="text" placeholder="Bank Code" required></input>
             </div>
 
             <div class="input__box">
-              <span class="details">Email Address</span>
-              <input
-                type="text"
-                placeholder="Enter Email Address"
-                required
-              ></input>
+              <input type="text" placeholder="Account Number" required></input>
             </div>
           </div>
         </form>
